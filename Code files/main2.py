ba4e5719cc28b1d022b4cd3b74776838d9f3fd5b
eashwerlatchumana-code <eashwerlatchumana@gmail.com>
2025-10-10 15:07:40 -0400

    yes

--- Code files/main2.py
+++ Code files/main2.py
@@ -7,11 +7,12 @@ import os
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
 from langchain_pinecone.vectorstores import Pinecone, PineconeVectorStore
 from pinecone import Pinecone
 from dotenv import load_dotenv
 from langchain.chains.question_answering import load_qa_chain
+
 load_dotenv()
 
 
@@ -23,7 +24,7 @@ def read_doc(directory):
     return document
 
 doc = read_doc("Code files")
-(type(doc)) # This is a list type 
+ 
 #breaking into chunks 
 
 def chunks(docs, chuck_size = 400, chunck_overlap = 50):
@@ -32,18 +33,19 @@ def chunks(docs, chuck_size = 400, chunck_overlap = 50):
     return splited_doc
 
 changed_doc = chunks(docs = doc) #This is chunked document 
-print(type(changed_doc))
+
 #embedding technique of OPEN AI 
 try:
     embeddings=OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY"))
 except Exception as E: 
     print(f"{E}, issue with finding llm api")
-vectors =embeddings.embed_query("TEST")
-print(len(vectors))
+
+
 
 #Vecter Serch DB in pincone 
-vectorstore = PineconeVectorStore.from_documents(documents=changed_doc, embedding=embeddings, 
-                                                 index_name = "langchain", pinecone_api_key = os.getenv("PINECONE_API_KEY")
+
+vectorstore = PineconeVectorStore(embedding=embeddings, 
+                                                 index_name = "langchaintest1", pinecone_api_key = os.getenv("PINECONE_API_KEY")
                                                  )
 
 #cosine similarity, retrive results from vector db 
@@ -54,5 +56,18 @@ def retrive_query(query, k=2):
 
 #Using Load_qa_chain
 #Defining the llm i am going to use 
-llm = OpenAI(model="gpt-4o", temperature=0.5)
-chain = load_qa_chain()
+llm = ChatOpenAI(model="gpt-4o", temperature=0.5)
+chain = load_qa_chain(llm,chain_type="stuff" )
+
+#search the asnwer
+def retrive_ans(query):
+    docsearch = retrive_query(query) #This is the document that is going to fetched from the vector data base 
+    # print(docsearch)
+    response = chain.invoke({'input_documents' : docsearch, 'question':query})
+    return response 
+question = ''
+while question != 'end':
+    question = input("Enter a question you would like to ask about the short story: ")
+    answer = retrive_ans(question)
+    print("------------------------------------------------------------------------------------------------------------------------")
+    print(answer['output_text'])
